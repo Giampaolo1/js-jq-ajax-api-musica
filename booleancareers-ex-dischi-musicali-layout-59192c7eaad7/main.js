af30deadb5bec53fe33a3542d7419e9d8a0ddea4
Giampaolo1 <giampaolo.r@outlook.com>
2019-11-19 17:17:40 +0100

    Start

--- booleancareers-ex-dischi-musicali-layout-59192c7eaad7/main.js
+++ booleancareers-ex-dischi-musicali-layout-59192c7eaad7/main.js
@@ -1,3 +1,23 @@
+// DESCRIZIONE:
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+// Servendoci di handlebars stampiamo tutto a schermo. In questo momento non è importante la parte grafica.
+// Chiamata:
+// https://flynn.boolean.careers/exercises/api/array/music
+
+
+
+
 $(document).ready(function() {
-	//Code
+    $.ajax({
+      url: "https://flynn.boolean.careers/exercises/api/array/music",
+      method: "GET",
+      success: function(datiRitorno){
+      console.log(datiRitorno)
+      },
+      error: function(){
+        console.log("Errore");
+      }
+
+    });
+
 });
