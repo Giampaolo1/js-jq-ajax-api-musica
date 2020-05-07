ca46ce0ecabccc7c8ad1ee3c19cdad4ad4e6bb25
Giampaolo1 <55143439+Giampaolo1@users.noreply.github.com>
2020-05-07 22:27:29 +0200

    a

--- main.js
+++ main.js
@@ -1,50 +1,42 @@
-// DESCRIZIONE:
 // Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
 // Servendoci di handlebars stampiamo tutto a schermo. In questo momento non è importante la parte grafica.
-// Chiamata:
-// https://flynn.boolean.careers/exercises/api/array/music
-
-
 
 
 $(document).ready(function() {
-    $.ajax({
-      url: "https://flynn.boolean.careers/exercises/api/array/music",
-      method: "GET",
-      success: function(datiRitorno){
-      console.log(datiRitorno)
-
-      // stampare in pagina
-
-      // $(".prova").text(datiRitorno.response[0].title)
-      // devo fare un ciclo for sull array
-        for(var i = 0; i < datiRitorno.response.length; i++) {
-          // console.log(datiRitorno.response[i])
-          // $(".prova").text(datiRitorno.response[i].title)
-          // $(".prova").text(datiRitorno.response[i].poster)
-
-          // $(".prova").text(
-          //   datiRitorno.response[i].poster + "<br>" +
-          //   $(".prova").text(datiRitorno.response[i].title)
-          // )
-
-          $(".prova").append(
-            datiRitorno.response[i].poster + "<br>" +
-            datiRitorno.response[i].author + "<br>" +
-            datiRitorno.response[i].genre + "<br>" +
-            datiRitorno.response[i].title + "<br>" +
-            datiRitorno.response[i].year + "<br>"
-          )
-
-      // devo fare un ciclo for sull oggetto
 
 
-        };
-      },
-      error: function(){
-        console.log("Errore");
-      }
+  $.ajax({
 
-    });
+    url:"https://flynn.boolean.careers/exercises/api/array/music",
+    method: "GET",
+    success: function(data){
+      var album = data.response;
+      // console.log(album);
+
+
+      // handlebars
+      var source   = $("#entry-template").html();
+      var template = Handlebars.compile(source);
+
+      for (var i = 0; i < album.length; i++) {
+        // console.log('item', album[i]);
+
+        //compilo con handlbars il mio album
+        var context = album[i];
+        var html = template(context);
+        console.log(html);
+        //farò un append per metterlo in pagina con jquery
+        $(".cds-container").append(html);
+
+      };
+
+    },
+    error: function(error){
+      alert("errore");
+    },
+
+  });
+
+
 
 });
